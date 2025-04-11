744eabb453bf3f17eef9ff0469491bafa1a3bb37
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-10 19:42:46 -0500

    Check is a string is Palindrome

---
+++ src/main/java/methods/simple_method.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class simple_method {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(4,5));
+    }
+}
