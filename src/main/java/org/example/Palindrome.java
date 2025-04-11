107950349c6768a36d6a8ff46d9ca4072d96a1de
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-10 19:10:09 -0500

    Check is a string is Palindrome

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+        if (s.equals(reversed)) {
+            System.out.println(reversed + " is palindrome");
+        } else {
+            System.out.println(reversed + " is not palindrome");
+        }
+    }
+}
