6f3b0c3b817518aed86c4b4386a12488cd173444
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-12 11:53:02 -0500

    Exercising

---
+++ src/main/java/org/example/exercise.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static int processString(String input) {
+        int length = input.length();
+        for (int i = 0; i < input; i++) {
+            System.out.println(input.charAt(i));
+        }
+        return length;
+    }
+        public static void main(String[] args) {
+            System.out.println("Hello, world!");
+    }
+
+}
