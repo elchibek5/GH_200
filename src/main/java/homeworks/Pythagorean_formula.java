251f25907ed3a913fece4a4d21e1c5dc1c476bf7
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-11 20:36:02 -0500

    Using the Pythangorean

---
+++ src/main/java/homeworks/Pythagorean_formula.java
@@ -0,0 +1,14 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Pythagorean_formula {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println(c);
+    }
+}
