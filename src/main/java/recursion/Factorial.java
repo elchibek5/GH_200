5be0d3bb8fad4520d4345a18b52918e72c7608ba
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-11 20:34:48 -0500

    .

---
+++ src/main/java/recursion/Factorial.java
@@ -0,0 +1,16 @@
+package recursion; //Function which calls itself.
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+
+}
