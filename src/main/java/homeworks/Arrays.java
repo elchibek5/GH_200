da45a354fae16ec08004d5ad797b3feb8720f0bb
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-15 00:18:28 -0500

    Solving arrays question

---
+++ src/main/java/homeworks/Arrays.java
@@ -0,0 +1,17 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[sc.nextInt()];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+            if (i % 2 == 0) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
