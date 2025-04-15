67ddf8e072c1223fbfcae16396641294728fb3e2
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-15 01:23:26 -0500

    Solving previous and next question

---
+++ src/main/java/homeworks/previous_and_next.java
@@ -0,0 +1,16 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class previous_and_next {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int previous_n = a - 1;
+        int next_n = a + 1;
+        System.out.println("The next number for the number " + a + " is " + next_n + ".");
+        System.out.println("The previous number for the number " + a + " is " + previous_n + ".");
+
+    }
+}
