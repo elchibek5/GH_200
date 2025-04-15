e5827c6a442b89a2d2d0fe1f768c59d6b9ec8ebe
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-15 18:28:24 -0500

    Solving GH Academy problems

---
+++ src/main/java/homeworks/Electric_Watch.java
@@ -0,0 +1,14 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Electric_Watch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = (n / 60) % 24;
+        int m = (n % 60);
+
+        System.out.println(h + " " + m);
+    }
+}
