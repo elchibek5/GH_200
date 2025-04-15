5ab81f4b5513075458b0d69fcc46ce08a19e78f6
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-14 22:42:15 -0500

    Printing larger number.

---
+++ src/main/java/homeworks/Conditional_Operations.java
@@ -0,0 +1,16 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Conditional_Operations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b) {
+            System.out.println(a);;
+        } else {
+            System.out.println(b);
+        }
+    }
+}
