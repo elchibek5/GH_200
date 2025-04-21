24fd598339a7ab4e751e1fc613b15f40a7400c28
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-21 12:58:23 -0500

    Chess rook beat solution

---
+++ src/main/java/week_4/solvingProblems/rookBeat.java
@@ -0,0 +1,19 @@
+package week_4.solvingProblems;
+
+import java.util.Scanner;
+
+public class rookBeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (a == c || b == d) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
