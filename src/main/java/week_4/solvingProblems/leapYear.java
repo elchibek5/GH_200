16edf29095828d53eb0605b7ee16b9da1ee57cf4
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-21 12:55:33 -0500

    Find Leap Year

---
+++ src/main/java/week_4/solvingProblems/leapYear.java
@@ -0,0 +1,23 @@
+package week_4.solvingProblems;
+
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println("YES"); 
+        } else if (year % 100 == 0) {
+            System.out.println("NO");
+        } else if (year % 4 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+    }
+}
