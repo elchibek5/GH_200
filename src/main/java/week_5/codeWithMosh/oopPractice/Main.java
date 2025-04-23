0e23096e50248f4948f063c8f7413ab4041e01e7
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-22 22:48:31 -0500

    Solving GH problems

---
+++ src/main/java/week_5/codeWithMosh/oopPractice/Main.java
@@ -0,0 +1,11 @@
+package week_5.codeWithMosh.oopPractice;
+
+public class Main {
+    public static void main(String[] args) {
+        var employee = new Employee();
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+    }
+}
