9fb7676f2179d22336b02ea017be9ed18536d9a9
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-24 00:25:48 -0500

    Checking rook's possible moves in chess

--- src/main/java/week_4/solvingProblems/rookBeat.java
+++ src/main/java/week_4/solvingProblems/rookBeat.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class rookBeat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
 
-        if (a == c || b == d) {
+        if (x1 == x2 || y1 == y2) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
