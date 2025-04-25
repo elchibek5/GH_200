50c72dcb1ef9cff2f9e0dac4bb9d6f4ff879dd49
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-25 02:52:23 -0500

    Square Sum Program

---
+++ src/main/java/week_5/solving_problems/SquareSum.java
@@ -0,0 +1,16 @@
+package week_5.solving_problems;
+
+import java.util.Scanner;
+
+public class SquareSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i * i;
+        }
+        System.out.println(sum);
+    }
+}
