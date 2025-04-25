4a68abfc49cbc8e803ff0c164f5510af5e65ba9a
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-25 01:54:24 -0500

    Printing sum of provided nums

---
+++ src/main/java/week_5/solving_problems/Sum.java
@@ -0,0 +1,16 @@
+//package week_5.solving_problems;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
