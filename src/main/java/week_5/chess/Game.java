9d949bff5e3e54b091a16b9627d791cb609530ba
elchibekdastanov <elchibekdastanov@gmail.com>
2025-04-27 23:11:22 -0500

    Practicing OOP

--- src/main/java/week_5/chess/Game.java
+++ src/main/java/week_5/chess/Game.java
@@ -5,30 +5,34 @@ public class Game {
 
     Game() {
         this.board = new Piece[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
                 this.board[i][j] = new EmptyPiece();
             }
         }
     }
-
+    /**
+     * makeMove - makes a move, moves the piece from (x1, y1) to (x2, y2).
+     * All coordinates must be 0 based.
+     */
     void makeMove(int x1, int y1, int x2, int y2) {
         if (!board[x2][y2].name().equals(" ")) {
 
         }
         if (board[x1][y1].canMove(x2, y2)) {
-        };
+
+        }
     }
 
     void printBoard() {
         System.out.print('\t');
-        for (char c = 'A';  c <= 'H'; c++) {
+        for (char c = 65; c <= 72; ++c) {
             System.out.print(c + "" + '\t');
         }
         System.out.println();
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; ++i) {
             System.out.print((i + 1) + "" + '\t');
-            for (int j = 0; j < 8; j++) {
+            for (int j = 0; j < 8; ++j) {
                 System.out.print(board[i][j].name() + '\t');
             }
             System.out.println();
