a515a5ee2ff65315ceadfb424c8dfcc715a2fa8f
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-03 00:14:52 -0500

    Finding sum of greater numbers than previous number

---
+++ src/main/java/week_7/solving_problems/GreaterThenPrevious.java
@@ -0,0 +1,24 @@
+package week_7.solving_problems;
+
+import java.util.Scanner;
+
+public class GreaterThenPrevious {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[sc.nextInt()];
+
+        int count  = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[i - 1]) count++;
+
+        }
+
+        System.out.println(count);
+    }
+}
