65816692a08b5e5d2507f3b4bb6841a7a840b33e
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-02 23:57:54 -0500

    Counts Positive Numbers

---
+++ src/main/java/week_7/solving_problems/PositiveNumbers.java
@@ -0,0 +1,23 @@
+//package week_7.solving_problems;
+
+import java.util.Scanner;
+
+public class PositiveNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[sc.nextInt()];
+        int positive_nums = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int number : numbers) {
+            if (number >= 1) {
+                positive_nums++;
+            }
+        }
+        System.out.println(positive_nums);
+    }
+}
