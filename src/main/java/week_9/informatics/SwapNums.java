e9bf5dbb854c4efdb0d7e3505c93148c3c99bf65
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-16 00:22:01 -0500

    Swapping every two numbers

---
+++ src/main/java/week_9/informatics/SwapNums.java
@@ -0,0 +1,24 @@
+//package week_9.informatics;
+
+import java.util.Scanner;
+
+public class SwapNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[sc.nextInt()];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length - 1; i+=2) {
+            int current = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = current;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " ");
+        }
+    }
+}
