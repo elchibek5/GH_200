76d58039d5aa5f5edbdcda5325de8d8d55046dd2
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-20 02:26:19 -0500

    Practicing exceptions

--- src/main/java/week_9/practice/ReadFileException.java
+++ src/main/java/week_9/practice/ReadFileException.java
@@ -3,11 +3,18 @@ package week_9.practice;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadFileException {
     void readFile(String filename) throws FileNotFoundException {
         FileReader in = new FileReader(filename);
         BufferedReader br = new BufferedReader(in);
-        br.readLine();
+
+        try {
+            String s = br.readLine();
+            System.out.println(s);
+        } catch (IOException e) {
+            System.out.println("some IO error occured");
+        }
     }
 }
