e6d7160b025467c379865c0de22e1bacd94b61d7
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-21 08:00:00 -0500

    Const1

--- src/main/java/week_9/bigo/Const1.java
+++ src/main/java/week_9/bigo/Const1.java
@@ -1,6 +1,9 @@
 package week_9.bigo;
 
 public class Const1 {
+
+
+    // g(1) - always takes the same time regardless of array size
     public static int getElement(int[] array, int index) {
         return array[index];
     }
