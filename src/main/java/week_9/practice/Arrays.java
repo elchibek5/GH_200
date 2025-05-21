8496c0c79a1431a7e0700907a4b5f09be1b793c7
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-21 15:16:20 -0500

    Finding numbers of odd index

---
+++ src/main/java/week_9/practice/Arrays.java
@@ -0,0 +1,19 @@
+package week_9.practice;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = new int[sc.nextInt()];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+            if (i % 2 == 0) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
