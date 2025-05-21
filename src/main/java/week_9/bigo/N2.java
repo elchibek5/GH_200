5aa0d745324b414cd0d4b55292803e952833fb16
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-21 08:01:05 -0500

    N2 complexity

---
+++ src/main/java/week_9/bigo/N2.java
@@ -0,0 +1,19 @@
+//package week_9.bigo;
+//
+//public class N2 {
+//    public static void bubbleSort(int array) {
+//        int n = array.length;
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n - i - 1; j++) {
+//                if (array[j] > array[j + 1]) {
+//                    int temp = array[j];
+//                    array[j] = array[j + 1];
+//                    array[j + 1] = temp;
+//
+//                }
+//            }
+//        }
+//        // Time complexity: O(n^2) - we have two nested loop each potentially running n times
+//    }
+//}
