5f7020914ec471ba697470d1d074cc8069cd0abd
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-22 12:50:02 -0500

    Merge Sorting

---
+++ src/main/java/week_9/master_theorem/MergeSort.java
@@ -0,0 +1,42 @@
+package week_9.master_theorem;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+
+            merge(array, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] array, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (array[i] <= array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = array[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = array[j++];
+        }
+
+        for (int m = 0; m < temp.length; m++) {
+            array[left + m] = temp[m];
+        }
+    }
+}
