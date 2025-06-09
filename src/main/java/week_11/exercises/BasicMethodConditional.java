5321cd03ec4d9bb5459606347d72e36ce41e51cd
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-09 15:18:10 -0500

    Checking Leap Year

---
+++ src/main/java/week_11/exercises/BasicMethodConditional.java
@@ -0,0 +1,8 @@
+package week_11.exercises;
+
+public class BasicMethodConditional {
+    public static boolean isLeapYear(int year) {
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
