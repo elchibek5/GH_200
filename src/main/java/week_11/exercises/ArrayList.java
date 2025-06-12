eba6e37e291eba2da3875974bdff00bcee45fac8
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-11 23:20:20 -0500

    Using OOP to encapsulate classes

---
+++ src/main/java/week_11/exercises/ArrayList.java
@@ -0,0 +1,15 @@
+package week_11.exercises;
+
+import java.util.List;
+
+public class ArrayList {
+    public static List<String> filterStrings(List<String> strings) {
+        List<String> result = new ArrayList<>();
+        for (String str : strings) {
+            if (str != null && str.length() > 3 && Character.isUpperCase(str.charAt(0))) {
+                result.add(str);
+            }
+        }
+        return result;
+    }
+}
