7e35fc0e7a58f46a736171452de19ea5e580ffbb
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-12 19:15:43 -0500

    Finding dividable factors

---
+++ src/main/java/week_11/exercises/FIndFactors.java
@@ -0,0 +1,14 @@
+package week_11.exercises;
+
+public class FIndFactors {
+    public static void main(String[] args) {
+        int n = 0;
+        int factors = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % 1 == 0) {
+                factors++;
+                System.out.println("n is dividable by " + i);
+            }
+        }
+    }
+}
