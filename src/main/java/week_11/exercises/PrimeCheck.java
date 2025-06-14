b63b685b04722849061de31f4c9a86a2ce5193b6
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-13 20:53:24 -0500

    Checks if the number is prime

---
+++ src/main/java/week_11/exercises/PrimeCheck.java
@@ -0,0 +1,12 @@
+package week_11.exercises;
+
+public class PrimeCheck {
+
+    boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
