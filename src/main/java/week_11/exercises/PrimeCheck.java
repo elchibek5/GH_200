d5231fde04461c7c808359350e12c2371a298447
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-13 20:55:16 -0500

    Checks if the number is prime

--- src/main/java/week_11/exercises/PrimeCheck.java
+++ src/main/java/week_11/exercises/PrimeCheck.java
@@ -1,7 +1,7 @@
 package week_11.exercises;
 
 public class PrimeCheck {
-
+    // It's not an algorithm, so it takes a lot of memory
     boolean isPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i * i <= n; i++) {
