721771eadad434a6d1fe79027e011c909c13e15d
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-13 21:02:43 -0500

    Two implemented codes both Checks if the number is prime, the second one take less memory.

--- src/main/java/week_11/exercises/PrimeCheck.java
+++ src/main/java/week_11/exercises/PrimeCheck.java
@@ -1,5 +1,7 @@
 package week_11.exercises;
 
+import java.util.Arrays;
+
 public class PrimeCheck {
     // It's not an algorithm, so it takes a lot of memory
     boolean isPrime(int n) {
@@ -8,5 +10,24 @@ public class PrimeCheck {
             if (n % i == 0) return false;
         }
         return true;
+    }
+
+    // This code takes the was less memory than the first one.
+    void sieveOfEratosthenes(int n) {
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+        primes[0] = primes[1] = false;
+
+        for(int p = 2; p * p <= n; n++) {
+            if (primes[p]) {
+                for (int i = p * p; i <= n; i += p) {
+                    primes[i] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (primes[i]) System.out.println(i);
+        }
     }
 }
