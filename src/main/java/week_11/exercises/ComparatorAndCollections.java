2639102ea125c44f81be94619810bef8c76417b9
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-16 15:48:18 -0500

    Comparing student's grade and age

---
+++ src/main/java/week_11/exercises/ComparatorAndCollections.java
@@ -0,0 +1,21 @@
+package week_11.exercises;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorAndCollections {
+    record Student(String name, double grade, int age) {
+
+        @Override
+        public String toString() {
+            return String.format("Student{name='%s', grade=%.1f), age=%d}", name, grade, age);
+        }
+    }
+
+    public static void sortStudents(List<Student> students) {
+        students.sort(
+                Comparator.comparingDouble(Student::grade).reversed()
+                        .thenComparingInt(Student::age)
+        );
+    }
+}
