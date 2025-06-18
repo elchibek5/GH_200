1f71dcee2518d753f5b377a16384d4f37cffb028
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-18 16:19:29 -0500

    Calculating total area of Circle, Rectangle and Triangle

---
+++ src/main/java/week_11/AreaCalculate.java
@@ -0,0 +1,24 @@
+package week_11;
+
+import week_5.interfaces.Circle;
+
+import java.awt.*;
+import java.util.List;
+
+public class AreaCalculate {
+    public static double calculteTotalArea(List<Shape> shapes) {
+        return shapes.stream()
+                .mapToDouble(shape -> {
+                    if (shape instanceof Circle c) {
+                        return c.area();
+                    } else if (shape instanceof Rectangle r) {
+                        return r.area();
+                    } else if (shape instanceof Triangle t) {
+                        return t.area();
+                    } else {
+                        throw new IllegalArgumentException("Unknown shape type");
+                    }
+                })
+                .sum();
+    }
+}
