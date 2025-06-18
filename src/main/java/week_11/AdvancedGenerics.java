de490c3760a026ac9c58a5acdd09bc2512f08fe1
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-18 16:33:41 -0500

    Advanced Generics - Bounded Type Parameters

---
+++ src/main/java/week_11/AdvancedGenerics.java
@@ -0,0 +1,37 @@
+package week_11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AdvancedGenerics {
+    static class MinMaxFinder<T extends Comparable<T>> {
+        private final List<T> elements = new ArrayList<>();
+
+        public void add(T element) {
+            if (element != null) {
+                elements.add(elements);
+            }
+        }
+
+        public Optional<T> getMin() {
+            return elements.stream().min(Comparable:: compareTo);
+        }
+
+        public Optional<T> getMax() {
+            return elements.stream().max(Comparable::compareTo);
+        }
+
+        public int size() {
+            return elements.size();
+        }
+
+        public void clear() {
+            elements.clear();
+        }
+
+        public List<T> getElements() {
+            return new ArrayList<>(elements);
+        }
+    }
+}
