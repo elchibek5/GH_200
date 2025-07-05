6d29c3055d252d3dd0c67fd9adb9005242322c7c
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-05 02:19:04 -0500

    Using Advanced Concepts

--- src/main/java/week_5/interfaces/Circle.java
+++ src/main/java/week_5/interfaces/Circle.java
@@ -1,7 +1,16 @@
 package week_5.interfaces;
 
-public class Circle implements Drawable, Printable{
+public class Circle implements Shape, Drawable {
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;  // Formula for area of a circle: πr²
+    }
 
     @Override
     public void draw() {
