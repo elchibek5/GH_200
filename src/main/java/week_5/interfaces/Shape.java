199e1a2c17d31ba2a99bbd16b0b3ab2e04b9581b
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-05 02:18:23 -0500

    Stream API

---
+++ src/main/java/week_5/interfaces/Shape.java
@@ -0,0 +1,7 @@
+package week_5.interfaces;
+
+public interface Shape {
+    double area();  // All shapes should implement this method
+    void draw();    // If you want to include drawing functionality
+    void print();   // If you want to include printing functionality
+}
