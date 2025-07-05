cd6ecb17483753d692af4e9086204353d9d09374
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-05 02:19:00 -0500

    Using Advanced Concepts

--- src/main/java/week_5/interfaces/Paint.java
+++ src/main/java/week_5/interfaces/Paint.java
@@ -2,7 +2,8 @@ package week_5.interfaces;
 
 public class Paint {
     public static void main(String[] args) {
-        Drawable c = new Circle();
+        // Provide the radius as an argument when creating the Circle object
+        Drawable c = new Circle(5.0); // Example radius of 5.0
         c.draw();
         c.print();
     }
