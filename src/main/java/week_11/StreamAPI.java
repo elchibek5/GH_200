04dea455f5b77f38b3f3592ba0430dac64e13cd5
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-05 02:18:17 -0500

    Stream API

--- src/main/java/week_11/StreamAPI.java
+++ src/main/java/week_11/StreamAPI.java
@@ -2,6 +2,7 @@ package week_11;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors; // Add this import
 
 public class StreamAPI {
     public static List<Integer> processNumbers(List<Integer> numbers) {
@@ -9,6 +10,6 @@ public class StreamAPI {
                 .filter(n -> n % 2 == 0)
                 .map(n -> n * n)
                 .sorted(Collections.reverseOrder())
-                .collect(Collections.toList());
+                .collect(Collectors.toList()); // Correct this line
     }
 }
