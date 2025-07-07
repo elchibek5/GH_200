d28374e4a486c48a1bc0dfb8142e5c0a886fa5fb
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-07 02:22:00 -0500

    Removes dublicates from String

---
+++ src/main/java/week_13/RemoveDublicates.java
@@ -0,0 +1,15 @@
+package week_13;
+
+public class RemoveDublicates {
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains(String.valueOf(str.charAt(i)))) {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+}
