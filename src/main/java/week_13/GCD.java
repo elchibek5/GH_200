fdb4047ed8a4031c6140ba95ddafb6caa5ab4ba5
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-10 02:32:04 -0500

    Finding Greatest Common Devisor

--- src/main/java/week_13/GCD.java
+++ src/main/java/week_13/GCD.java
@@ -22,6 +22,7 @@ public class GCD {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int sqrt = (int) Math.floor(Math.sqrt(n));
+        boolean first_print = true;
         for (int i = 2; i <= sqrt; i++) {
             int count = 0;
             while (n % i == 0) {
@@ -29,11 +30,24 @@ public class GCD {
                 n /= i;
             }
             if (count > 0) {
+                if (first_print) {
+                    first_print = false;
+                } else {
+                    System.out.println("*");
+                }
                 System.out.println(i);
                 if (count > 1) {
                     System.out.println("^" + count);
                 }
             }
+        }
+        if (n > 1) {
+            if (first_print) {
+                first_print = false;
+            } else {
+                System.out.println("*");
+            }
+            System.out.println(n);
         }
     }
 }
