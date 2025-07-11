6aa1ad143440c551301f675b3699546577c30da8
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-11 18:03:54 -0500

    ASCII

---
+++ src/main/java/week_13/ASCII.java
@@ -0,0 +1,12 @@
+package week_13;
+
+public class ASCII {
+    public static void main(String[] args) {
+        System.out.println((int)'0');
+        System.out.println((int)'1');
+        System.out.println((int)'2');
+        System.out.println((int)'9');
+        System.out.println((int)'A');
+
+    }
+}
