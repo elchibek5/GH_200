0a18e77f8f03a2ac41b2719888498dc343a88634
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-11 18:24:23 -0500

    Doing math

---
+++ src/main/java/week_13/SImpleMath.java
@@ -0,0 +1,12 @@
+package week_13;
+
+public class SImpleMath {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        int c = 0;
+
+        c = ++ b + a + b ++;
+        System.out.println(c);
+    }
+}
