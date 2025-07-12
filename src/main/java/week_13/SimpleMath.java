6f7971e92af9a089cdd33718ae17d60fcf080493
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-11 19:40:35 -0500

    Finding the last array element

--- src/main/java/week_13/SimpleMath.java
+++ src/main/java/week_13/SimpleMath.java
@@ -1,9 +1,15 @@
 package week_13;
 
 public class SimpleMath {
-    static String s;
-    public static void main(String[] args) {
-        System.out.println(">>" + s + "<<");
+    public static int getLastElement(int[] array) throws IllegalAccessException {
+        if (array == null || array.length == 0) {
+            throw new IllegalAccessException("Array is empty or null");
         }
+        return array[array.length - 1];
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        int[] nums = {1, 3, 6, 4, 7};
+        System.out.println("The last element: " + getLastElement(nums));
     }
 }
