12f31b7ae69658a5457df512a159d466c6fb7804
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-26 22:51:27 -0500

    String emojis

--- src/main/java/week_3/methods/string_concatenate.java
+++ src/main/java/week_3/methods/string_concatenate.java
@@ -1,11 +1,22 @@
 package week_3.methods;
 
+import java.util.Random;
+
 public class string_concatenate {
     public static String concatenate(String string, int num) {
-        return string + num;
+        String[] emojis = {"😊", "🚀", "🔥", "🎉", "😎", "💡", "🧠"};
+        Random rand = new Random();
+        String emojiTrail = "";
+
+        for (int i = 0; i < num % 5; i++) {
+            emojiTrail += emojis[rand.nextInt(emojis.length)];
+        }
+
+        return string + num + " " + emojiTrail;
     }
 
     public static void main(String[] args) {
         System.out.println(concatenate("Hello ", 45));
+        System.out.println("Did you know? 45 is " + (45 % 2 == 0 ? "even 🔢" : "odd 🧮"));
     }
 }
