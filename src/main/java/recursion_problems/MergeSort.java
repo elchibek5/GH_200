f5fa0fb92cf15bff2255d7a151ee284c630df00a
elchibekdastanov <elchibekdastanov@gmail.com>
2025-10-24 17:21:51 -0500

    Merge Sorting

---
+++ src/main/java/recursion_problems/MergeSort.java
@@ -0,0 +1,50 @@
+package recursion_problems;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] data = new int[]{-5, 10, 2, 43, 10, 4};
+        System.out.println("Before: " + Arrays.toString(data));
+        mergeSort(data, 0, data.length - 1);
+        System.out.println("After " + Arrays.toString(data));
+    }
+
+    public static void mergeSort(int[] data, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(data, start, mid);
+            mergeSort(data,mid + 1, end);
+            merge(data, start, mid, end);
+        }
+    }
+
+    public static void merge(int[] data, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+
+        int i = start, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= end) {
+            if (data[i] <= data[j]) {
+                temp[k] = data[i];
+                i++;
+                k++;
+            } else {
+                temp[k++] = data[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = data[i++];
+        }
+
+        while (j <= end) {
+            temp[k++] = data[j++];
+        }
+
+        for (i = start; i <= end; i++) {
+            data[i] = temp[i - start];
+        }
+    }
+
+}
