a6930234d0daf92c285d5f99258c8c6e2837d1f0
elchibekdastanov <elchibekdastanov@gmail.com>
2025-10-23 23:51:49 -0500

    Converting decimal num to binary

---
+++ src/main/java/recursion_problems/decimalToBinary.java
@@ -0,0 +1,25 @@
+package recursion_problems;
+
+import java.util.Scanner;
+
+public class decimalToBinary {
+    public String convert(int num, String result) {
+        if (num == 0) {
+            return result;
+        }
+
+        result = num % 2 + result;
+        return convert(num / 2, result);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+
+        decimalToBinary dc = new decimalToBinary();
+        String input = dc.convert(num, "");
+
+        System.out.println(input);
+    }
+
+}
